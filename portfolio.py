07bb72a6ad90a7c767d11a992a4e76ff6f79233c
Wenyu Dai <u7988619@anu.edu.au>
2026-09-06 01:12:59 +1000

    Added linkedin link to portfolio

--- portfolio.py
+++ portfolio.py
@@ -1,12 +1,24 @@
 from pydantic import BaseModel
 
+class LinkedIn(BaseModel):
+    prefix: str
+    label: str
+    url: str
+
 class Portfolio(BaseModel):
     intro: str
     content: str
+    linkedin: LinkedIn
 
 portfolio = Portfolio(
     intro = "Hi, my name is Wenyu.",
     content = "I'm an experienced software engineer with a Master of Computing (Advanced) from the Australian National University. \n\n" \
               "Beyond software engineering, I'm passionate about data analysis, education, and research. \n\n" \
-              "Have a look around to learn more about my experience and work."
+              "Have a look around to learn more about my experience and work. \n\n",
+    linkedin = LinkedIn(
+        prefix = "You're also welcome to connect with me on",
+        label = "LinkedIn",
+        url = "https://www.linkedin.com/in/wenyu-dai/"
+    )
 )
+
